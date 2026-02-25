272694067e146ec595a3f676a691ba9daa5acde2
Yomafil <128907736+Yomafil@users.noreply.github.com>
2026-02-25 04:48:06 +0100

    Add cover

--- plugin/js/parsers/69shuParser.js
+++ plugin/js/parsers/69shuParser.js
@@ -213,5 +213,9 @@ class _69shuTwParser extends ShuParser {
 
     findChapterTitle(dom) {
         return dom.querySelector("#nr_title");
-    }   
+    }
+    
+    findCoverImageUrl(dom) {
+        return util.getFirstImgSrc(dom, ".bookinfo table tbody tr td");
+    }
 }
